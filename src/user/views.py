73c64ee9f3eec42faac81e5dcf5ca3b4ea6edc75
Giga Amiridze <flameyungoxx@gmail.com>
2022-09-20 19:11:10 +0400

    Added signup page content and styles

--- src/user/views.py
+++ src/user/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import user_blueprint
+from .forms import Signup, Signin
 
-@user_blueprint.route('/signup')
+@user_blueprint.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    return render_template('signup.html')
+    form = Signup()
+    return render_template('signup.html', form=form)
