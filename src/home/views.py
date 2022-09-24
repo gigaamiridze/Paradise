0c348feb6b6e4717959376cfb738df8f87952259
Giga Amiridze <flameyungoxx@gmail.com>
2022-09-24 18:58:56 +0400

    Added pages title generator using macro

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from . import home_blueprint
+from src.resources import pages
 
 @home_blueprint.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', pages=pages)
---
+++ src/resources/pages.py
@@ -0,0 +1,14 @@
+pages = [
+    {
+        "endpoint": "home.home_page",
+        "title": "Paradise"
+    },
+    {
+        "endpoint": "user.sign_up",
+        "title": "Sign Up"
+    },
+    {
+        "endpoint": "user.sign_in",
+        "title": "Sign In"
+    },
+]
