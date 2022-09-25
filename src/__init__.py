b3e6fae1ada212632fa949ba07952a09dcce974a
Giga Amiridze <flameyungoxx@gmail.com>
2022-09-25 08:49:35 +0400

    Added extensions

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from .config import Config
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    register_extensions(app)
     register_blueprints(app)
     return app
 
+
+def register_extensions(app):
+    # Setup Flask-SQLAlchemy
+    db.init_app(app)
+
+    # Setup Flask-Migrate
+    migrate.init_app(app, db)
 
 def register_blueprints(app):
     from src.home.views import home_blueprint
