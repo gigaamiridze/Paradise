2704e09d701f41b696f1cd0edb239edc82bd52ab
Giga Amiridze <flameyungoxx@gmail.com>
2022-09-26 18:51:28 +0400

    Added dashboard's other templates and routes

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -7,3 +7,31 @@ from src.resources import pages
 @login_required
 def dash_home():
     return render_template('home.html', pages=pages)
+
+@dashboard_blueprint.route('/musics')
+def musics():
+    return render_template('musics.html', pages=pages)
+
+@dashboard_blueprint.route('/albums')
+def albums():
+    return render_template('albums.html', pages=pages)
+
+@dashboard_blueprint.route('/artists')
+def artists():
+    return render_template('artists.html', pages=pages)
+
+@dashboard_blueprint.route('/favourites')
+def favourites():
+    return render_template('favourites.html', pages=pages)
+
+@dashboard_blueprint.route('/analytics')
+def analytics():
+    return render_template('analytics.html', pages=pages)
+
+@dashboard_blueprint.route('/events')
+def events():
+    return render_template('events.html', pages=pages)
+
+@dashboard_blueprint.route('/add-music', methods=['GET', 'POST'])
+def add_music():
+    return render_template('add-music.html', pages=pages)
--- src/resources/pages.py
+++ src/resources/pages.py
@@ -15,4 +15,32 @@ pages = [
         "endpoint": "dashboard.dash_home",
         "title": "Paradise - Web Player"
     },
+    {
+        "endpoint": "dashboard.musics",
+        "title": "Paradise - Musics"
+    },
+    {
+        "endpoint": "dashboard.albums",
+        "title": "Paradise - Albums"
+    },
+    {
+        "endpoint": "dashboard.artists",
+        "title": "Paradise - Artists"
+    },
+    {
+        "endpoint": "dashboard.favourites",
+        "title": "Paradise - Favourites"
+    },
+    {
+        "endpoint": "dashboard.analytics",
+        "title": "Paradise - Analytics"
+    },
+    {
+        "endpoint": "dashboard.events",
+        "title": "Paradise - Events"
+    },
+    {
+        "endpoint": "dashboard.add_music",
+        "title": "Paradise - Add Music"
+    }
 ]
